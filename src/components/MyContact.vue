87df8d19d28c731093701c52e14aed48187b18e0
SyedAbdullah58-dev <syedabdullah199758@gmail.com>
2023-12-03 22:37:52 +0500

    contact section added.

---
+++ src/components/MyContact.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+</script>
+
+<template>
+  <footer>
+  <v-col >
+    <div content="center" id="contact">
+      <v-row >
+        <v-col style="padding-top: 60px"><v-row>
+          <v-col  align-self="center" align="center"><v-btn
+              icon="fas fa-map-marker-alt"
+              color="black"
+              class="mt-20"
+              variant="outlined"
+          ></v-btn
+          ><br />
+            <span class="text-black" style="font-weight: bold">Rawalpindi, Pakistan</span
+            ><br /></v-col>
+          <v-col align-self="center" align="center">
+            <v-btn
+                icon="fas fa-phone-alt"
+                color="black"
+                class="mt-20"
+                variant="outlined"
+            ></v-btn
+            ><br />
+            <span class="text-black">+923341157461 </span> <br /></v-col>
+          <v-col align-self="center" align="center">
+            <v-btn
+                icon="fas fa-envelope"
+                color=""
+                class="mt-20"
+                variant="outlined"
+            ></v-btn
+            ><br />
+            <span class="text-black">syedabdullah199758@gmail.com </span> <br /></v-col></v-row></v-col>
+
+      </v-row>
+    </div>
+  </v-col></footer>
+</template>
+
+<style scoped>
+#contact{
+border-radius: 30px;
+  background-color: #edf0f5;
+  margin-top: 20px;
+  font-weight: bold;
+}
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -18,8 +18,8 @@
    </v-col>
    <v-col cols="5">
      <div style="position: relative; z-index: 9999" class="mt-16">
-
-       <v-img class="profileimg" src="dp.jpg" contain max-height="300"></v-img>
+       <v-avatar  class="profile avatar-shadow" color="grey" size="300">
+         <v-img  src="web3.jpg" ></v-img></v-avatar>
      </div>
    </v-col>
  </v-row>
@@ -113,16 +113,39 @@
   <v-col cols="12" class="imgHover">  <v-row class="fill-width" align=center justify="space-between">
     <template v-for="(item,i) in items" :key="i">
       <v-col cols="12" sm="4">
-        <v-hover  v-slot="{isHovering,props}"><v-card elevation="isHovering?12:2" :class="{'on-hover':isHovering}" v-bind="props">
-<v-img :src="item.img" height="100vh"
+        <v-hover  v-slot="{isHovering,props}"><v-card  :class="{'on-hover':isHovering}" v-bind="props">
+<v-img :src="item.img"
         cover></v-img>
         </v-card></v-hover>
-
       </v-col>
-
     </template>
   </v-row >
   </v-col>
+
+  <v-col cols="12" sm="12">
+    <div class="d-flex justify-center mb-6">
+      <v-btn color="#FBDF7E" class="mt-4">Load More</v-btn>
+    </div>
+  </v-col>
+  <v-divider></v-divider>
+  <v-col cols="12" id="biog">
+    <div class="hire">
+      <v-row>
+        <v-col cols="12" sm="8">
+          <h1 class="mt-9">Hire me for your awesome project</h1>
+          <p class="text-grey">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+            itaque, eaque molestiae deleniti, earum voluptate eos id dicta
+            at, blanditiis
+          </p>
+        </v-col>
+        <v-col cols="12" sm="4">
+          <v-btn color="#FBDF7E" class="mt-15">Hire Me</v-btn>
+        </v-col>
+      </v-row>
+    </div>
+  </v-col>
+<MyContact></MyContact>
 
 </v-container>
 </v-app>
@@ -135,6 +158,8 @@ import { defineComponent } from 'vue';
 
 import NavBar from "@/components/NavBar.vue";
 
+import MyContact from "@/components/MyContact.vue";
+
 export default defineComponent({
   name: 'HomeView',
 setup(){
@@ -142,6 +167,15 @@ setup(){
       slider2:50,
       items: [
         {
+          img: "web1.jpg",
+        },
+        {
+          img: "web2.jpg",
+        },
+        {
+          img: "web3.jpg",
+        },
+          {
           img: "s1.jpg",
         },
         {
@@ -164,6 +198,8 @@ setup(){
 
 },
   components: {
+    MyContact,
+
     NavBar,
   },
 });
@@ -215,11 +251,15 @@ setup(){
   margin-left: 150px;
   margin-top: 50px;
   border-radius: 50%;
-  width: 300px; /* Adjust the width and height to your preference */
-  height: 300px; /* Should be the same as the width for a perfect circle */
+  height: 300px;
+  width: 300px;
+  /* Adjust the width and height to your preference */
+   /* Should be the same as the width for a perfect circle */
   /* Ensures the image covers the entire circle */
   border: 2px solid #fff;
-  object-fit: fill;
+  background: aqua;
+  object-fit: initial;
+
 }
 .first {
   width: 100%;
@@ -244,8 +284,14 @@ setup(){
   padding: 0 200px;
   background-color: #f5f5f5;
 }
+.hire {
+  width: 110%;
+  height: 200px;
+  padding: 0 200px;
+  background-color: #e9e9e9;
+  margin-top: 20px;
+}
 #services{
-
 width: 300px;
 }
 </style>
