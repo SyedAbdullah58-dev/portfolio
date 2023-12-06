675ce7e2c89bd0afea4f5dc57fecfaa661edbac3
SyedAbdullah58-dev <syedabdullah199758@gmail.com>
2023-12-06 12:38:04 +0500

    deployment changes

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,19 @@
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import '@fortawesome/fontawesome-free/css/all.css'
+import {aliases,fa} from 'vuetify/iconsets/fa'
+// Vuetify
+import { createVuetify } from 'vuetify'
+
+export default createVuetify(
+  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+    {
+        icons:{
+            defaultSet:'fa',aliases,sets:{
+                fa,
+            }
+        }
+
+    }
+)
